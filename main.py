99a64d73b8c258a020083b68f3bbd9d3d3096c46
Diego Cândido <diegocndd4@gmail.com>
2023-03-08 13:08:48 -0300

    second commit

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ api_hash = '1ae533d7273bd5824e6a5a29c46cfc8f'
 ENTRY_LINK = 'https://t.me/canaltestee0'
 
 with TelegramClient('name', api_id, api_hash) as client:
-    id_cloned_channel = '1542598247'
+    id_cloned_channel = '1824797129'
 
     @client.on(events.NewMessage(pattern='(?i).*'))
     async def handler(event):
+
         destiny_channel = await client.get_entity(ENTRY_LINK)
 
         if (str(event.message.peer_id.channel_id) == id_cloned_channel):
